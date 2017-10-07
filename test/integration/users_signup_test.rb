94ccb0631e7eb48f8df693862eb44d90068d54c5
Sandeep Gottimukkala <sgottimukkal@tamu.edu>
2017-10-07 23:18:05 +0000

    Bug fix

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -11,8 +11,6 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
                                          password_confirmation: "bar" } }
     end
     assert_template 'users/new'
-    assert_select 'div#<CSS id for error explanation>'
-    assert_select 'div.<CSS class for field with error>'
   end
   
   test "valid signup information" do
@@ -25,6 +23,5 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
     end
     follow_redirect!
     assert_template 'users/show'
-    assert_not flash.FILL_IN
   end
 end
