ff7233bdfc53dd11b1b83e3a64a7dd64f99876e9
Satya Kesav <gskesav@tamu.edu>
2017-11-01 07:44:48 +0000

    micropost_to_posts proper_news_feed

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -5,7 +5,7 @@ class MicropostsController < ApplicationController
   def create
     @micropost = current_user.microposts.build(micropost_params)
     if @micropost.save
-      flash[:success] = "Micropost created!"
+      flash[:success] = "Post created!"
       redirect_to root_url
     else
       @feed_items = []
@@ -15,7 +15,7 @@ class MicropostsController < ApplicationController
 
   def destroy
     @micropost.destroy
-    flash[:success] = "Micropost deleted"
+    flash[:success] = "Post deleted"
     redirect_to request.referrer || root_url
   end
   
