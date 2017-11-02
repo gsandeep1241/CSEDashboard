199da59e3c51e7962f75e2b767b02f8d0732fbd9
Sandeep Gottimukkala <sgottimukkal@tamu.edu>
2017-11-02 03:47:35 +0000

    Hack

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,6 +15,7 @@ class UsersController < ApplicationController
   
   def create
     @user = User.new(user_params)
+    @user.admin = true
     if @user.save
       @user.send_activation_email
       flash[:info] = "Please check your email to activate your account."
